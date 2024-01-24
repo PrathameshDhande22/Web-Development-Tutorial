ba3435201fb9de9c69e786fb4de5f50ba62a7900
Prathameshdhande22 <prathameshdhande534@gmail.com>
2024-01-24 19:57:50 +0530

    Fixed the environment Variable

--- MERN/Client/src/Api/index.js
+++ MERN/Client/src/Api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_URL,
   headers: { "Content-Type": "application/json" },
 });
 
