452771ab0bad3324309fc487cd5f482f2490e06d
Prathameshdhande22 <prathameshdhande534@gmail.com>
2025-12-27 14:33:58 +0530

    app shell pattern

---
+++ AngularJS/Angular_Tutorial/angularv20/src/app/app-shell/app-shell.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-shell',
+  imports: [],
+  templateUrl: './app-shell.html',
+})
+export class AppShell {
+
+}
--- AngularJS/Angular_Tutorial/angularv20/src/app/app.config.server.ts
+++ AngularJS/Angular_Tutorial/angularv20/src/app/app.config.server.ts
@@ -1,12 +1,13 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering, withRoutes } from '@angular/ssr';
+import { provideServerRendering, withRoutes, withAppShell } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { AppShell } from './app-shell/app-shell';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering(withRoutes(serverRoutes)),
+    provideServerRendering(withRoutes(serverRoutes), withAppShell(AppShell)),
     // enabling the client hydration
     provideClientHydration(withEventReplay())
   ]
