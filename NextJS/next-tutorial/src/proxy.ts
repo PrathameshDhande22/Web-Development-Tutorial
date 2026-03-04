a6d0408a5b6e0120d6ed77d1bfedcd27b7a4a6ab
Prathameshdhande22 <prathameshdhande534@gmail.com>
2026-03-05 05:06:41 +0530

    Middleware Added

--- NextJS/next-tutorial/src/proxy.ts
+++ NextJS/next-tutorial/src/proxy.ts
@@ -1,13 +1,13 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse, ProxyConfig } from "next/server";
 
 export function proxy(request: NextRequest) {
   const response = NextResponse.next();
   // Setting the Cookie
-  response.cookies.set("Data", "Value");
+  response.cookies.set("Data", "ValueFromMiddleware");
   return response;
 }
 
 // adding the matcher to match the routes on where to apply the middleware
-export const config = {
-  matcher: "/comments/:path",
+export const config: ProxyConfig = {
+  matcher: "/comments/:path*",
 };
